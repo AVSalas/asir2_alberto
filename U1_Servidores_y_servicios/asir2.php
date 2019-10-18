24f04dd15b844ed39d666477bcacbabb4ca4cbe3
AVSalas <albertodelvalle1992@gmail.com>
2019-10-18 23:54:30 +0200

    Correción de asir2.php y subida del archivo visitas.txt.

--- U1_Servidores_y_servicios/asir2.php
+++ U1_Servidores_y_servicios/asir2.php
@@ -1,11 +1,17 @@
-Alberto del Valle.
 
-<br/>
-<a href="http://192.168.0.201/asir2_alvaro/asir2.php">Alvaro</a>
-<br/>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>asir2.php</title>
+  </head>
+  <body>
+    Alberto del Valle.
+    <br/>
+      <a href="http://192.168.0.201/asir2_alvaro/asir2.php">Alvaro</a>
+  </body>
+</html>
 
 <?php
-phpinfo();
 //****** Script de David ******//
 $f=fopen('visitas.txt','a');
 fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
