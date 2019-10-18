5f8976c083121021498462397daf209ce3771295
AVSalas <albertodelvalle1992@gmail.com>
2019-10-19 01:16:13 +0200

    smr2b y visitas con timestamp.

---
+++ U1_Servidores_y_servicios/smr2b.php
@@ -0,0 +1,19 @@
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>asir2.php</title>
+  </head>
+  <body>
+    Alberto del Valle.
+    <br/>
+      <a href="http://192.168.0.201/asir2_alvaro/asir2.php">Alvaro</a>
+  </body>
+</html>
+
+<?php
+//****** Script de David ******//
+$f=fopen('visitas.txt','a');
+fwrite($f,$_SERVER['REMOTE_ADDR'].', '.date("h:i:sa")."\r\n");
+fclose($f)
+?>
