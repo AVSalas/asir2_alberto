a06eb10438a7d750f9477e0a3e21938827f78c18
AVSalas <albertodelvalle1992@gmail.com>
2019-11-24 04:12:10 +0100

    2a, variables.

---
+++ U2_Backend/2a.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Variables</title>
+  </head>
+  <body>
+    <p>Variables</p>
+    <?php
+      $a=100;
+      $b=100;
+        echo 'Numero A = '.$a.' y Numero B = '.$b;
+          echo '</br>';
+          echo '</br>';
+        echo 'Suma A + B: '.$a.' + '.$b.' = '.($a+$b);
+          echo '</br>';
+          echo '</br>';
+        echo 'Resta A - B: '.$a.' - '.$b.' = '.($a-$b);
+          echo '</br>';
+          echo '</br>';
+        echo 'Multiplica A x B: '.$a.' x '.$b.' = '.($a*$b);
+          echo '</br>';
+          echo '</br>';
+        echo 'Divide A / B: '.$a.' / '.$b.' = '.($a/$b);
+          echo '</br>';
+      ?>
+  </body>
+</html>
