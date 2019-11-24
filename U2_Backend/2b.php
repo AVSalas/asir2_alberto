4b0cce3a8b9297d40765625ff763a5b560eea0d6
AVSalas <albertodelvalle1992@gmail.com>
2019-11-24 04:20:13 +0100

    2b, funciones.

---
+++ U2_Backend/2b.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Funciones</title>
+  </head>
+  <body>
+    <p>Funciones</p>
+    <?php
+    function sumresmuldiv() {
+      $a=100;
+      $b=100;
+        echo 'Numero A = '.$a.' y Numero B = '.$b;
+          echo '</br>';
+          echo '</br>';
+        echo 'Suma A + B: '.$a.' + '.$b.' = '.($a+$b);
+          echo '</br>';
+          echo '</br>';
+        echo 'Resta A - B: '.$a.' - '.$b.' = '.($a-$b);
+          echo '</br>';
+          echo '</br>';
+        echo 'Multiplica A x B: '.$a.' x '.$b.' = '.($a*$b);
+          echo '</br>';
+          echo '</br>';
+        echo 'Divide A / B: '.$a.' / '.$b.' = '.($a/$b);
+          echo '</br>';
+      }
+      sumresmuldiv();
+      ?>
+  </body>
+</html>
