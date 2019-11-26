e61a92880a49ea84a154d5eda7f8bdaa3b41321a
AVSalas <albertodelvalle1992@gmail.com>
2019-11-26 01:38:56 +0100

    Actividad HTML.

---
+++ U3_Frontend/html.php
@@ -0,0 +1,64 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>HTML</title>
+  </head>
+  <body>
+    <style>
+      body {
+        background-color:#c6ffb8;
+      }
+      td {
+        background-color: cyan;
+        font-size: 25px;
+      }
+      #dv {
+          margin: 5px 5px 5px 5px;
+      }
+    </style>
+    <div id="dv">
+      <h1>Tamaño vista de abuelo (H1)</h1>
+      <h2>Venga, que sabes que se ve bien (H2)</h2>
+      <h3>Un texto sorprendentemente normal (H3)</h3>
+      <h4>Igual necesitas acercarte (H4)</h4>
+      <h5>You'll need <i>culo botella</i> glasses (H5)</h5>
+      <h6>Las dioptrias bien, ¿no? (H6)</h6>
+    <center>
+      <p>Imagen</p>
+      <img src="pajaro.png" width="400" height="280" >
+      <br/>
+      <p><b>A donde vas</b>, <u>manzanas traigo</u>.
+    </center>
+        <form>
+          Campo1 <input type="text" name="Campo1" placeholder="Introduce algo anda" autofocus/>
+          Campo2 <input type="text" name="Campo2" placeholder="que no cuesta nada"/>
+          <input type="submit" value="Pointless button"/> (Mmmhn...) Si no has pillado la referencia, clica <a href="https://www.youtube.com/watch?v=4aK3P-n1aWk">aqui</a>
+        </form>
+        <br/>
+        <table border="1px">
+            <th colspan="2">Provincias bañadas por el Cantabrico</th>
+            <tr>
+              <td>Galicia</td>
+              <td>Cantabria</td>
+            </tr>
+            <tr>
+              <td>Asturias</td>
+              <td>Euskadi</td>
+            </tr>
+        </table>
+        <br/>
+        <font color="orange">
+          Texto naranja.
+        </font>
+        <font color="red">
+          Texto rojo.
+        </font>
+        <font color="white">
+          Texto blanco.
+        </font>
+        <font color="magenta">
+          Texto magenta.
+        </font>
+    </div>
+  </body>
+</html>
