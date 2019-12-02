b2aa1e966bcc8e6841b6007b039365c1cecf65b5
AVSalas <albertodelvalle1992@gmail.com>
2019-12-02 16:04:47 +0100

    html revisado.

--- U3_Frontend/html.php
+++ U3_Frontend/html.php
@@ -1,21 +1,9 @@
 <html>
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8"/>
     <title>HTML</title>
   </head>
-  <body>
-    <style>
-      body {
-        background-color:#c6ffb8;
-      }
-      td {
-        background-color: cyan;
-        font-size: 25px;
-      }
-      #dv {
-          margin: 5px 5px 5px 5px;
-      }
-    </style>
+  <body bgcolor="#c6ffb8">
     <div id="dv">
       <h1>Tamaño vista de abuelo (H1)</h1>
       <h2>Venga, que sabes que se ve bien (H2)</h2>
@@ -25,38 +13,38 @@
       <h6>Las dioptrias bien, ¿no? (H6)</h6>
     <center>
       <p>Imagen</p>
-      <img src="pajaro.png" width="400" height="280" >
+      <img src="pajaro.png" width="400" height="280"/>
       <br/>
-      <p><b>A donde vas</b>, <u>manzanas traigo</u>.
+      <p><b>A donde vas</b>, <u>manzanas traigo</u></p>.
     </center>
         <form>
-          Campo1 <input type="text" name="Campo1" placeholder="Introduce algo anda" autofocus/>
-          Campo2 <input type="text" name="Campo2" placeholder="que no cuesta nada"/>
-          <input type="submit" value="Pointless button"/> (Mmmhn...) Si no has pillado la referencia, clica <a href="https://www.youtube.com/watch?v=4aK3P-n1aWk">aqui</a>
+          Campo1: <input type="text" name="Campo1" placeholder="Introduce algo anda" autofocus/>
+          Campo2: <input type="text" name="Campo2" placeholder="que no cuesta nada"/>
+          <input type="submit" value="Pointless button"/> (Mmmhn...) Si no has pillado la referencia, clica <a href="https://www.youtube.com/watch?v=4aK3P-n1aWk">aqui</a>.
         </form>
         <br/>
-        <table border="1px">
+        <table border>
             <th colspan="2">Provincias bañadas por el Cantabrico</th>
-            <tr>
+            <tr bgcolor="cyan">
               <td>Galicia</td>
               <td>Cantabria</td>
             </tr>
-            <tr>
+            <tr bgcolor="cyan">
               <td>Asturias</td>
               <td>Euskadi</td>
             </tr>
         </table>
         <br/>
-        <font color="orange">
+        <font size="15" color="orange">
           Texto naranja.
         </font>
-        <font color="red">
+        <font size="20" color="red">
           Texto rojo.
         </font>
-        <font color="white">
+        <font size="25"color="white">
           Texto blanco.
         </font>
-        <font color="magenta">
+        <font size="30" color="magenta">
           Texto magenta.
         </font>
     </div>
