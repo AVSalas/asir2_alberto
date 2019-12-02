ea536754e62fd44c1083cfe734b27c620f9c11ba
AVSalas <albertodelvalle1992@gmail.com>
2019-12-02 15:49:25 +0100

    Triangulos c revisado.

--- U2_Backend/triangulos_c.php
+++ U2_Backend/triangulos_c.php
@@ -1,6 +1,5 @@
 <?php
-function triangulosc ($a,$b,$c,$d,$e,$f) {
-
+function triangulos_a($a,$b,$c) {
   if ($a==$b and $a==$c) {
     $producto='Triangulo equilatero';
   } elseif ($a==$b or $a==$c or $b==$c) {
@@ -8,16 +7,24 @@ function triangulosc ($a,$b,$c,$d,$e,$f) {
   } else {
     $producto='Triangulo escaleno';
   }
-
+  return $producto;
+}
+function triangulos_b($a,$b,$c) {
   if ($a==90 or $b==90 or $c==90) {
 				$resultado='rectangulo';
 			} elseif($a>90 or $b>90 or $c>90) {
 				$resultado='obtusangulo';
 			} else
 				$resultado='acutangulo';
-        return $producto.' y '.$resultado.'. ';
+        return $resultado;
 }
-    echo triangulosc(100,100,100,100,100,100);
-    echo triangulosc(100,150,100,200,300,250);
-    echo triangulosc(250,170,210,150,100,80);
+function triangulos_c ($a,$b,$c,$d,$e,$f) {
+	return triangulos_a($a,$b,$c).' y '.triangulos_b($d,$e,$f);
+}
+echo triangulos_c(100,100,100,100,100,100).'<br/>';
+echo triangulos_c(100,150,100,200,300,250).'<br/>';
+echo triangulos_c(250,170,210,150,100,80).'<br/>';
+echo triangulos_c(1,1,1,90,45,45).'<br/>';
+echo triangulos_c(1,1,1,60,60,60).'<br/>';
+echo triangulos_c(250,170,210,150,100,80).'<br/>';
 ?>
