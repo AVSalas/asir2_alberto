93725d99ad9561858f00f0bb3ddf46330a8dc3ba
AVSalas <albertodelvalle1992@gmail.com>
2020-01-20 17:11:17 +0100

    Triangulos B - Alberto.

--- U2_Backend/triangulos_b.php
+++ U2_Backend/triangulos_b.php
@@ -8,5 +8,8 @@ function triangulos_b($a,$b,$c) {
 				$resultado='acutangulo';
         return $resultado;
       }
-      echo triangulos_b(30,45,68);
+      echo triangulos_b(90,90,90)."<br/>";
+      echo triangulos_b(100,120,140)."<br/>";
+      echo triangulos_b(30,50,70)."<br/>";
+
 ?>
