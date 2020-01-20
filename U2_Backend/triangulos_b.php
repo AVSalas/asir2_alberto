c2774dcbc9590f3f874c3d6bd1677edcb6bead95
AVSalas <albertodelvalle1992@gmail.com>
2020-01-20 17:24:22 +0100

    Triangulos B - Alberto.

--- U2_Backend/triangulos_b.php
+++ U2_Backend/triangulos_b.php
@@ -1,5 +1,8 @@
 <?php
 function triangulos_b($a,$b,$c) {
+  if($a+$b+$c>180) {
+    echo "Triangulo no valido";
+  } else {
   if ($a==90 or $b==90 or $c==90) {
 				$resultado='rectangulo';
 			} elseif($a>90 or $b>90 or $c>90) {
@@ -8,8 +11,9 @@ function triangulos_b($a,$b,$c) {
 				$resultado='acutangulo';
         return $resultado;
       }
-      echo triangulos_b(90,90,90)."<br/>";
-      echo triangulos_b(100,120,140)."<br/>";
+  }
+      echo triangulos_b(90,90,0)."<br/>";
+      echo triangulos_b(100,60,20)."<br/>";
       echo triangulos_b(30,50,70)."<br/>";
 
 ?>
